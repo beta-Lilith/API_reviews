1ddb6ae39f8bdb92230bd1fd6f479147b5bb426a
Ирина Баронская <barons42@yandex.ru>
2023-06-29 18:24:28 +0300

    Добавлены сериализаторы для Произведений, Жанров, Категорий

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
 
 from reviews.models import (
-    User,
-    CODE_LENGTH, REGEX, USER_NAME_LENGTH
+    Category, Genre, Title, User,
+    CODE_LENGTH, REGEX, USER_NAME_LENGTH,
 )
-
+from .validators import validate_slug, validate_year
 
 FORBIDDEN_NAME = 'Имя "me" использовать нельзя!'
 
@@ -33,3 +34,59 @@ class TokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'confirmation_code',)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField(
+        validators=[validate_slug]
+    )
+
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField(
+        validators=[validate_slug]
+    )
+
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+
+
+class ShowTitleSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(
+        read_only=True,
+    )
+    genre = GenreSerializer(
+        read_only=True,
+        many=True
+    )
+    # rating = serializers.IntegerField(read_only=True, source='')
+
+    class Meta:
+        model = Title
+        fields = (
+            'id', 'name', 'year', 'description', 'genre', 'category'
+        )
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    category = SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all()
+    )
+    genre = SlugRelatedField(
+        slug_field='slug',
+        queryset=Genre.objects.all(),
+        many=True
+    )
+    year = serializers.IntegerField(
+        validators=[validate_year]
+    )
+
+    class Meta:
+        model = Title
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category')

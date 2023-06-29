a203833c1ebeaa4c1b35d6e66a6477a9ca1512b1
Ирина Баронская <barons42@yandex.ru>
2023-06-29 18:12:38 +0300

    Добавлены Вьюсеты Произведений, Жанров, Категорий

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, mixins, viewsets
+from rest_framework.pagination import LimitOffsetPagination
 
-# Create your views here.
+from rewiews.models import Category, Genre, Title
+# from .permissions import IsAdminOrReadOnly
+from .serializers import (CategorySerializer, GenreSerializer,
+                          ShowTitleSerializer, TitleSerializer)
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = (ShowTitleSerializer, TitleSerializer)
+    # permission_classes = (IsAdminOrReadOnly, )
+    pagination_class = LimitOffsetPagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('category__slug', 'genre__slug', 'name', 'year')
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return ShowTitleSerializer
+        return TitleSerializer
+
+
+class CategoryViewSet(mixins.CreateModelMixin,
+                      mixins.DestroyModelMixin,
+                      mixins.ListModelMixin,
+                      viewsets.GenericViewSet,):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    # permission_classes = (IsAdminOrReadOnly,)
+    pagination_class = LimitOffsetPagination
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=name', )
+    lookup_field = 'slug'
+
+
+class GenreViewSet(mixins.CreateModelMixin,
+                   mixins.DestroyModelMixin,
+                   mixins.ListModelMixin,
+                   viewsets.GenericViewSet,):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    # permission_classes = (IsAdminOrReadOnly,)
+    pagination_class = LimitOffsetPagination
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=name', )
+    lookup_field = 'slug'

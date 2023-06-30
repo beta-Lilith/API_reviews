336e735bd728e9db390d3182df263bd031f7fecc
Kseniia_O <liprekqn@gmail.com>
2023-07-01 00:28:40 +0300

    Version for review AUTH serializers-urls

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -3,10 +3,18 @@ from rest_framework import serializers
 from reviews.models import User
 
 
+FORBIDDEN_NAME = 'Имя "me" использовать нельзя!'
+
+
 class SignUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email',)
+
+    def validate_username(self, name):
+        if name == 'me':
+            raise serializers.ValidationError(FORBIDDEN_NAME)
+        return name
 
 
 class TokenSerializer(serializers.ModelSerializer):

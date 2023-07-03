adef002fda06d6d4058f8db0c8527aafb2cdaefc
Павел Рванцев <rvpaul132@gmail.com>
2023-07-03 11:45:01 +0300

    сериалайзеры для review,comment

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,14 +1,44 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.generics import get_object_or_404
+from rest_framework.exceptions import ValidationError
 
 from reviews.models import (
-    Category, Genre, Title, User,
+    Category, Genre, Title, User, Comment,
+    Review,
     CODE_LENGTH, REGEX, USER_NAME_LENGTH,
 )
 from .validators import validate_year
 
 
 FORBIDDEN_NAME = 'Имя "me" использовать нельзя!'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    title = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True,
+    )
+    author = serializers.SlugRelatedField(
+        default=serializers.CurrentUserDefault(),
+        slug_field='username',
+        read_only=True
+    )
+
+    def validate(self, data):
+        request = self.context['request']
+        author = request.user
+        title_id = self.context['view'].kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if request.method == 'POST':
+            if Review.objects.filter(title=title, author=author).exists():
+                raise ValidationError('Вы не можете добавить более'
+                                      'одного отзыва на произведение')
+        return data
+
+    class Meta:
+        model = Review
+        fields = '__all__'
 
 
 class SignUpSerializer(serializers.ModelSerializer):
@@ -84,3 +114,18 @@ class TitleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Title
         fields = ('id', 'name', 'year', 'description', 'genre', 'category')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    review = serializers.SlugRelatedField(
+        slug_field='text',
+        read_only=True
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        model = Comment
+        fields = '__all__'

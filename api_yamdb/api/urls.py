e67e1370686641d263f1e1b5f96f92b1e93f1f05
Павел Рванцев <rvpaul132@gmail.com>
2023-07-04 21:08:35 +0300

    урлы для review,comment

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -19,10 +19,10 @@ router_v1 = routers.DefaultRouter()
 router_v1.register('titles', TitleViewSet, basename='titles')
 router_v1.register('categories', CategoryViewSet, basename='categories')
 router_v1.register('genres', GenreViewSet, basename='genres')
-router.register(r'titles/(?P<title_id>\d+)/reviews',
-                ReviewViewSet, basename='reviews')
-router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)'
-                r'/comments', CommentViewSet, basename='comments')
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews',
+                   ReviewViewSet, basename='reviews')
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)'
+                   r'/comments', CommentViewSet, basename='comments')
 
 # users ???
 

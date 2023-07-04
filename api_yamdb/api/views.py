bd45433edfbe2ea7a3095bb068f89c288754ce68
Павел Рванцев <rvpaul132@gmail.com>
2023-07-04 23:34:59 +0300

    changes

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework import filters, mixins, status, serializers, viewsets
 from rest_framework_simplejwt.tokens import AccessToken
 
-from reviews.models import Category, Genre, Title, User, Review 
+from reviews.models import Category, Genre, Title, User, Review
 from .serializers import (CategorySerializer, GenreSerializer,
                           TitleSerializer, ShowTitleSerializer,
                           SignUpSerializer, TokenSerializer)
@@ -102,7 +102,7 @@ class GenreViewSet(mixins.CreateModelMixin,
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
-    permission_classes = [IsAdminModeratorOwnerOrReadOnly]
+    # permission_classes
 
     def get_queryset(self):
         title = get_object_or_404(Title, pk=self.kwargs.get("title_id"))
@@ -110,14 +110,15 @@ class ReviewViewSet(viewsets.ModelViewSet):
         return title.reviews.all()
 
     def perform_create(self, serializer):
-        title_id = self.kwargs.get('title_id')
-        title = get_object_or_404(Title, id=title_id)
+        title = get_object_or_404(Title,
+                                  id=self.kwargs.get('title_id'))
+
         serializer.save(author=self.request.user, title=title)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [IsAdminModeratorOwnerOrReadOnly]
+    # permission_classes
 
     def get_queryset(self):
         review = get_object_or_404(Review, pk=self.kwargs.get("review_id"))

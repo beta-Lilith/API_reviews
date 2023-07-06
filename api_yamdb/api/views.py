4cb06f7c0f58c7e260ff1e6c2d34e897ebc9cb05
Kseniia_O <liprekqn@gmail.com>
2023-07-06 23:27:05 +0300

    Correction in views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -9,7 +9,6 @@ from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import (
     AllowAny,
     IsAuthenticated,
-    IsAuthenticatedOrReadOnly,
 )
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
@@ -166,10 +165,7 @@ class GenreViewSet(CategoryGenreViewSet):
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
-    permission_classes = (
-        IsAuthenticatedOrReadOnly,
-        IsAdminOrModeratorOrAuthorOrReadOnly,
-    )
+    permission_classes = (IsAdminOrModeratorOrAuthorOrReadOnly,)
 
     def get_queryset(self):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
@@ -182,10 +178,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (
-        IsAuthenticatedOrReadOnly,
-        IsAdminOrModeratorOrAuthorOrReadOnly,
-    )
+    permission_classes = (IsAdminOrModeratorOrAuthorOrReadOnly,)
 
     def get_queryset(self):
         review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
